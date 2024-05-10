017578d9947ef3e45d2896e8eb3bf8aa204f0412
vlada <narl0durn0@ya.ru>
2024-05-10 19:15:37 +0300

    added get user ID from request

--- internal/contoller/http/helpers.go
+++ internal/contoller/http/helpers.go
@@ -3,6 +3,9 @@ package http
 import (
 	"fmt"
 	"github.com/google/uuid"
+	"github.com/hell-kitchen/backend/internal/model"
+	"github.com/labstack/echo/v4"
+	"net/http"
 )
 
 func (ctrl *Controller) generateAccessAndRefreshForUser(user uuid.UUID) (access string, refresh string, err error) {
@@ -15,4 +18,22 @@ func (ctrl *Controller) generateAccessAndRefreshForUser(user uuid.UUID) (access 
 		return "", "", fmt.Errorf("error while creating refresh token: %w", err)
 	}
 	return
+}
+
+func (ctrl *Controller) getUserIDFromRequest(c echo.Context) (uuid.UUID, error) {
+	var (
+		cookie   *http.Cookie
+		err      error
+		userData *model.UserDataInToken
+	)
+	cookie, err = c.Cookie(ctrl.cfg.AccessCookieName)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("error while getting access cookie: %w", err)
+	}
+
+	userData, err = ctrl.token.GetDataFromToken(cookie.Value)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("error while getting access token: %w", err)
+	}
+	return userData.ID, nil
 }
